ec883b1bc9045b1497380751ff7be6ba855b875b
Joerg Steinkamp <joerg.steinkamp@senckenberg.de>
2016-01-20 09:41:47 +0100

    No errors any more for 'R CMD check RVCtools'

--- datasets/GFED4.1s.R
+++
@@ -1,8 +0,0 @@
-# use the RVCTools v2.9
-source("~/Tools/RVCTools/v2.9/rvc-tools.R")
-
-BA <- stack("HDF5:/data/forrest/Fire/GFED4.1s/Monthly/GFED4.1s_1997.hdf5://ancill/basis_regions")      
-
-BA[BA == 0] <- NA
-
-BA.HD <- aggregate(BA, fun = modal, na.rm  = TRUE)
