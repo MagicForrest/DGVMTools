0d7f76f199b752523bfda0ddd31f1707bf2821ba
MagicForrest <spogman@gmail.com>
2018-01-10 17:19:54 +0100

    Removed old ssplot version of plotSpatial, moved the AGBtoTotalCarbon conversion to DGVMData package.

--- R/010-misc.R
+++ R/010-misc.R
@@ -580,25 +580,6 @@ reversed.fire.palette <- grDevices::colorRampPalette(rev(c("red4", "red","orange
 reversed.tim.colors = function(n) rev(fields::tim.colors(n))
 
 
-########### CONVERSION OF ABOVE-GROUND BIOMASS TO TOTAL CARBON ####################
-#' Calculate total Carbon given above ground biomass.
-#'
-#' Equation from Baccini et al . 2012. 
-#' Used when reading the original Baccini et al. 2012 and Avitabile et al. 2015 dataset
-#' 
-#' @param AGB Above ground biomass (carbon)
-#' 
-#' @author Matthew Forrest \email{matthew.forrest@@senckenberg.de}
-AGBtoTotalCarbon <- function(AGB){
-  
-  BGB <- 0.489 * AGB^0.89
-  total.carbon <- (AGB + BGB) / 2
-  return(total.carbon)
-  
-}
-
-
-
 
 
 ## define for older R versions, where this function does not exist.
