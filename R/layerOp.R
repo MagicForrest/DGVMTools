9be91dba6080869be5c2508d4ebc063ae4d0fce0
MagicForrest <matthew.forrest@senckenberg.de>
2021-01-07 17:21:35 +0100

    Fixed error in layerOp() documentation.

--- R/layerOp.R
+++ R/layerOp.R
@@ -10,7 +10,7 @@
 #'  \item{"mean" (or "average")} {Average the layers, can apply to any number of layers}
 #'  \item{"*" (or "multiply" or "product")} {Multiply the layers, can apply to any number of layers}
 #'  \item{"-" (or "subtract" or "minus")} {Subtract one layer from another. Requires exactly two layers to be specified, subtracts the second from the first, ie. layer1 - layer 2.}
-#'  \item{"/" (or "divide" or "through")} {Divide one layer by another. Nore this does 'safe division' which returns zero if the denomination is zero. Requires exactly two layers to be specified, divides the second by the first, ie. layer1 / layer 2.}
+#'  \item{"/" (or "divide" or "through")} {Divide one layer by another. Nore this does 'safe division' which returns zero if the denomination is zero. Requires exactly two layers to be specified, divides the first by the second, ie. layer1 / layer 2.}
 #'  \item{"max.layer"} {Gets the layer with the maximum value from the input layers (is a layer of factors).  If they are all zero at a point then "None" is assigned. In the case of ties, the first layer in the layers arguement will be returned as the max.}
 #'  \item{"min.layer"} {Gets the layer with the minimum value from the input layers (is a layer of factors).  If they are all zero at a point then "None" is assigned. In the case of ties, the first layer in the layers arguement will be returned as the min.}
 #'  \item{"mulc"/"divc"/"addc"/"subc"} {Multiplies, divides, adds or subtracts the layer with a numeric value in the "constant" argument}
